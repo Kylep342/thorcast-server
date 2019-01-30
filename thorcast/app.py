640b908877630d977645ab3d1c8c592d19baf679
Kylep342 <kylep342@gmail.com>
2019-01-29 20:12:27 -0600

    Removal of Rust (at least for now) Renamed 'python_poc' to 'thorcast' Added build.sh and run.sh for Docker'

--- thorcast/app.py
+++ thorcast/app.py
@@ -4,14 +4,14 @@ import thorcast
 def main():
     print('Welcome to Thorcast!')
     while True:
-        address = input('Please enter the city and state, separated by a comma, to retrieve a forecast.\n')
+        address = input('\nPlease enter the city and state, separated by a comma, to retrieve a forecast.\n')
         try:
             city, state = list(map(lambda x: x.strip(), address.split(',')))
         except ValueError:
-            print('Please enter city, then state, separated by a comma (",")')
+            print('Please enter city, then state, separated by a comma (",")\n')
             continue
-        print(thorcast.thorcast(city, state))
-        another = input('Would you like to check another forecast? [y/n]\n')
+        print('\n\n\n', thorcast.thorcast(city, state), sep='', end='\n\n\n')
+        another = input('\nWould you like to check another forecast? [y/n]\n')
         if another.lower() in ['n', 'no']:
             exit()
         else:
