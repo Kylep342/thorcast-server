e4990ee44af92dd81b9b59c69f78748a0c68b432
Kylep342 <kylep342@gmail.com>
2019-04-06 17:32:34 -0500

    Implemented 404 HTTP error

--- utils/errors.py
+++ utils/errors.py
@@ -1,4 +1,6 @@
-
+"""
+Module containing all API error classes
+"""
 
 class ServerError(Exception):
     status_code = 500
@@ -18,6 +20,22 @@ class ServerError(Exception):
 
 class ClientError(Exception):
     status_code = 400
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['message'] = self.message
+        return rv
+
+
+class NotFoundError(Exception):
+    status_code = 404
 
     def __init__(self, message, status_code=None, payload=None):
         Exception.__init__(self)
